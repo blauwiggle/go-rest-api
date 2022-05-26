c1fa47ec39df78535b848c90f93883b262979dcc
Michael Vanhee <michael.vanhee@icloud.com>
2022-05-26 16:25:58 +0200

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/blauwiggle/go-rest-api/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers to the database, etc.
 type App struct{}
@@ -8,6 +13,15 @@ type App struct{}
 // Run - sets up our application and starts the server
 func (app *App) Run() error {
 	fmt.Println("Starting up our REST API")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to start our REST API")
+		return err
+	}
+
 	return nil
 }
 
